c1f7ba87f589e7272c21d030a18add4e29ee3873
Ivan_Kustau <Ivan_Kustau@epam.com>
2022-08-29 18:26:07 +0300

    EPMRPP-68413 || Service configuration

---
+++ src/main/java/com/epam/reportportal/ComplexMigrationsApplication.java
@@ -0,0 +1,14 @@
+package com.epam.reportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplexMigrationsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ComplexMigrationsApplication.class, args);
+	}
+
+}
